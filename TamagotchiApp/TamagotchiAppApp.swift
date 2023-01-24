01f342811ae72c3255deb50318a5e6870f37277a
whopes <hopes.w@etoncollege.org.uk>
2023-01-24 10:16:43 +0000

    Initial Commit

---
+++ TamagotchiApp/TamagotchiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiAppApp.swift
+//  TamagotchiApp
+//
+//  Created by Hopes, William (AMM) on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
